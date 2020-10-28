703c01114108ab0cbe3accb2dd168091df438eae
Niall Nulty <nnulty97@gmail.com>
2020-10-28 19:34:02 +0000

    created AWS asset that allows user to input AWS details

---
+++ AWS-S3Bucket-ReadDownloadUpload/Assets/Scripts/AWSConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AWSConfig", menuName = "AWS/Create AWSConfig", order = 1)]
+public class AWSConfig : ScriptableObject
+{
+    #region Properties
+    public string AWSKey;
+    public string AWSSecretKey;
+    public string AWSS3BucketName;
+    #endregion
+}
